d3218fa39925e4526692dd47d518739224ad8378
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2023-11-09 13:02:30 +0700

    update

--- src/main/java/com/example/sd18204/servlet/SinhVienServlet.java
+++ src/main/java/com/example/sd18204/servlet/SinhVienServlet.java
@@ -9,20 +9,53 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 @WebServlet(name = "SinhVienServlet", value = {
-        "/sinh-vien/hien-thi",
+        "/sinh-vien/hien-thi", // get
         "/sinh-vien/add",
+        "/sinh-vien/detail", //get
 
 })
 public class SinhVienServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ArrayList<SinhVien> list = new ArrayList<>();
+    ArrayList<SinhVien> list = new ArrayList<>();
+    ArrayList<String> lops = new ArrayList<>();
+
+    public SinhVienServlet() {
         list.add(new SinhVien("1", "SV1", "Nguyen Van A", "SD12345", "Nam"));
         list.add(new SinhVien("2", "SV2", "Nguyen Van B", "SD12346", "Nam"));
         list.add(new SinhVien("3", "SV3", "Nguyen Van C", "SD12347", "Nu"));
         list.add(new SinhVien("4", "SV4", "Nguyen Van D", "SD12345", "Nam"));
+        lops.add("SD12345");
+        lops.add("SD12346");
+        lops.add("SD12347");
+        lops.add("SD12349");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uri = request.getRequestURI();
+        if (uri.contains("/hien-thi")) {
+            this.getList(request, response);
+        } else if (uri.contains("/detail")) {
+            this.detail(request, response);
+        }
+    }
+
+    private void detail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        SinhVien sinhVienDetail = new SinhVien();
+        for (SinhVien sinhVien : list) {
+            if (sinhVien.getId().equals(id)) {
+                sinhVienDetail = sinhVien;
+            }
+        }
+        request.setAttribute("sinhVien", sinhVienDetail);
+        request.getRequestDispatcher("/detail.jsp").forward(request, response);
+    }
+
+    private void getList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("lops", lops);
         request.setAttribute("listSinhVien", list);
         request.getRequestDispatcher("/hien-thi-sinh-vien.jsp").forward(request, response);
+
     }
 
     @Override
--- src/main/webapp/detail.jsp
+++ src/main/webapp/detail.jsp
@@ -1,27 +1,29 @@
 <%--
   Created by IntelliJ IDEA.
   User: nguyenvv
-  Date: 02/11/2023
-  Time: 12:49
+  Date: 09/11/2023
+  Time: 12:26
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 <body class="container">
 <form>
     <div class="mb-3">
         <label class="form-label">ID</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" value="${sinhVien.id}">
     </div>
     <div class="mb-3">
         <label class="form-label">Ma</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" value="${sinhVien.ma}">
     </div>
     <div class="mb-3">
         <label class="form-label">Ho Ten</label>
@@ -48,38 +50,5 @@
 
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-<p>Danh sach sinh vien</p>
-<table class="table">
-    <thead>
-    <tr>
-        <td>STT</td>
-        <td>ID</td>
-        <td>Ma</td>
-        <td>Ho ten</td>
-        <td>Lop</td>
-        <td>Gioi Tinh</td>
-        <td>Action</td>
-    </tr>
-    </thead>
-    <tbody>
-
-    <c:forEach var="i" items="${listSinhVien}" varStatus="sinhVien">
-        <tr>
-            <td>${sinhVien.index}</td>
-            <td>${i.id}</td>
-            <td>${i.ma}</td>
-            <td>${i.hoTen}</td>
-            <td>${i.lop}</td>
-            <td>${i.gioiTinh}</td>
-            <td>
-                <a href="#" class="btn btn-primary">Xoa</a>
-                <a href="/sinh-vien/detail?id=${i.id}" class="btn btn-primary">Detail</a>
-            </td>
-        </tr>
-    </c:forEach>
-    </tbody>
-</table>
-
-
 </body>
 </html>
